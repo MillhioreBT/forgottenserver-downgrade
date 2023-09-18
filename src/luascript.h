c2157aca1bf7c337df2eaeccd5555ff0a395f6fc
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-09-18 09:17:52 -0400

    Update luascript.h

--- src/luascript.h
+++ src/luascript.h
@@ -323,7 +323,7 @@ public:
 		} else if (num < static_cast<lua_Number>(typename std::numeric_limits<T>::lowest())) {
 			reportErrorFunc(L,
 			                fmt::format("Argument {} has out-of-range value for {}: {}", arg, typeid(T).name(), num));
-			return std::numeric_limits<T>::lowest());
+			return std::numeric_limits<T>::lowest();
 		} else if (num > static_cast<lua_Number>(typename std::numeric_limits<T>::max())) {
 			reportErrorFunc(L,
 			                fmt::format("Argument {} has out-of-range value for {}: {}", arg, typeid(T).name(), num));
