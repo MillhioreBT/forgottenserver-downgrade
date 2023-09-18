7ed43584f74f71600cb5b9efe55ad5a105fbab7e
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-09-18 10:08:14 -0400

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -325,7 +325,7 @@ std::string formatDate(time_t time)
 		return {};
 	}
 
-	char buffer[20];
+	char buffer[50];
 	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900,
 	                  tms->tm_hour, tms->tm_min, tms->tm_sec);
 	if (res < 0) {
@@ -341,7 +341,7 @@ std::string formatDateShort(time_t time)
 		return {};
 	}
 
-	char buffer[12];
+	char buffer[30];
 	size_t res = strftime(buffer, 12, "%d %b %Y", tms);
 	if (res == 0) {
 		return {};
