2fcbc43086a06ca71a428e82f18226c89223250b
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-09-18 10:05:16 -0400

    Revert "Update tools.cpp"
    This reverts commit e86ce4b827e437023baf69ae8e494ad239a4ad77.

--- src/tools.cpp
+++ src/tools.cpp
@@ -320,14 +320,33 @@ std::string convertIPToString(uint32_t ip)
 
 std::string formatDate(time_t time)
 {
-	struct tm timeinfo = fmt::localtime(time);
-	return fmt::format("{:%d/%m/%Y %H:%M:%S}", &timeinfo);
+	const tm* tms = localtime(&time);
+	if (!tms) {
+		return {};
+	}
+
+	char buffer[20];
+	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900,
+	                  tms->tm_hour, tms->tm_min, tms->tm_sec);
+	if (res < 0) {
+		return {};
+	}
+	return {buffer, 19};
 }
 
 std::string formatDateShort(time_t time)
 {
-	struct tm timeinfo = fmt::localtime(time);
-	return fmt::format("{:%d %b %Y}", &timeinfo);
+	const tm* tms = localtime(&time);
+	if (!tms) {
+		return {};
+	}
+
+	char buffer[12];
+	size_t res = strftime(buffer, 12, "%d %b %Y", tms);
+	if (res == 0) {
+		return {};
+	}
+	return {buffer, 11};
 }
 
 Direction getDirection(const std::string& string)
